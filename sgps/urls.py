4202e9c73f078ad9f5b96d3dde2e92216f1d0e1f
areaujo <areaujo@5fae5908-39b6-1f9a-06cb-99cf62a38a6d>
2010-06-11 05:57:33 +0000

    Se agregaron las configuraciones para permisos y privilegios

--- sgps/urls.py
+++ sgps/urls.py
@@ -61,7 +61,8 @@ urlpatterns = patterns('',
     (r'^administracion/roles/editar/(\d+)/(\w+)/$', modificarRol),
     (r'^administracion/roles/eliminar/(\d+)/(\w+)/$', eliminarRoles),
     (r'^administracion/roles/permisos/(\d+)/(\w+)/$', GestionarPermisos),
-    (r'^administracion/roles/permisos/(\d+)/agregar$', agregar_permisos),    
+    (r'^administracion/roles/permisos/(\d+)/agregar$', agregar_permisos),
+    (r'^administracion/roles/privilegio/(\d+)/(\d+)/$', GestionarPrivilegios),    
     #Control de Tipo de Artefacto..........
 
     (r'^administracion/tipo_artefacto/$', TipoArtefactos),
